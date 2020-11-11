39b97efdc0c10126863a4aabb4901b300e09ca86
Jhonata Galante <jhonata.galante@gmail.com>
2020-11-11 09:47:23 -0300

    Fix button bugs

--- app/Controllers/Series.php
+++ app/Controllers/Series.php
@@ -1,21 +1,26 @@
-<?php namespace App\Controllers;
+<?php
+
+namespace App\Controllers;
 
 use App\Models\SerieModel;
 use CodeIgniter\Controller;
 
-class Series extends Controller {
+class Series extends Controller
+{
 
     protected $serieModel;
 
-    public function __construct(){
+    public function __construct()
+    {
         helper('form');
         $this->serieModel = new SerieModel();
     }
 
-    public function index(){
+    public function index()
+    {
 
         $series = $this->serieModel->findAll();
-        
+
         $variaveis = [
             'series' => $series
         ];
@@ -23,44 +28,44 @@ class Series extends Controller {
         return view('home/home_page', $variaveis);
     }
 
-    public function novaSerie(){
+    public function novaSerie()
+    {
         return view('nova_serie/home');
     }
 
-    public function sair(){
+    public function sair()
+    {
         die();
     }
 
-    public function store(){
+    public function store()
+    {
 
         $dados = $this->request->getPost();
 
         $request = $this->serieModel->save($dados);
 
-        if($request){
-            return view('nova_serie/sucesso_salvar_serie');
-        }else{
+        if ($request) {
+            return view('home/home_page', ['series' => $this->serieModel->findAll()]);
+        } else {
             return view('nova_serie/error_salvar_serie');
         }
     }
 
-    public function edit($id = null){
+    public function edit($id = null)
+    {
 
         $dado = $this->serieModel->find($id);
-        
+
         return view('nova_serie/home', ['serie' => $dado]);
-        
     }
 
-    public function delete($id = null){
-        
-        if($this->serieModel->delete($id)){
+    public function delete($id = null)
+    {
+        if ($this->serieModel->delete($id)) {
             return view('home/home_page', ['series' => $this->serieModel->findAll()]);
-        }else{
+        } else {
             return view('error_deletar_serie');
         }
-
     }
-
-
 }
--- app/Views/home/listagem.php
+++ app/Views/home/listagem.php
@@ -1,4 +1,23 @@
-<div class="container mt-5">
+<script>
+    function confirmDelete(id){
+        bootbox.confirm({
+            message:'<p class="text-dark">Tem certeza que deseja excluir essa série?</p>',
+            callback: function(confirmacao){
+                if(confirmacao){
+                    window.location.href= "<?php echo site_url('series/delete/')?>"+id;
+                }else{
+                    bootbox.alert('<p class="text-dark">Exclusão cancelada.</p>');
+                }
+            },
+            buttons: {
+                cancel: {label: '<i class="fa fa-times"></i> Cancelar', className:'btn-default'},
+                confirm: {label: '<i class="fa fa-check"></i> Confirmar', className:'btn-danger'}
+            }
+        });
+    }
+</script>
+
+<div class="container mt-4">
     <h3>Séries</h3>
     <table class="table table-hover table-light text-dark table-striped">
         <thead>
@@ -22,8 +41,8 @@
                     <td><?= $serie['episodios'] ?></td>
                     <td>
                         <div class="btn-group">
-                            <button class="btn btn-success mr-1"><?php echo anchor('series/edit/'.$serie['id'], 'Editar')?></button>
-                            <button class="btn btn-danger"><?php echo anchor('series/delete/'.$serie['id'], 'Excluir')?></button>
+                            <?php echo anchor('series/edit/'.$serie['id'], '<button class="btn btn-success mr-1">Editar</button>')?>
+                            <button class="btn btn-danger" onclick="confirmDelete(<?php echo $serie['id'] ?>)">Excluir</button>
                         </div>
                     </td>
                 </tr>
